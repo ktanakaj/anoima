6c5596997014ba732f3316fc219fdbf86f92f1fb
Koichi Tanaka <ktanakaj@users.noreply.github.com>
2017-01-03 13:08:52 +0900

    vagrant up等の注意書き追加と、読み込まれないライブラリがあったためその修正など

--- anoima-web/public/app/main.ts
+++ anoima-web/public/app/main.ts
@@ -1,6 +1,7 @@
 /**
  * @file あの人は今？ブートローダー。
  */
+import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/retry';
 import { enableProdMode } from '@angular/core';
